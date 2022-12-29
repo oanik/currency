775c0430f919ea43d642c0f2f92aff943f76d1f3
Olga Anikeieva <olga.anikeieva@gmail.com>
2022-12-29 23:32:52 +0200

    routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { Provider } from "react-redux";
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <Router basename='/currency'>
+    <Router >
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
